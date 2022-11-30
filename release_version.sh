2d346dd548cc46903a9723e98315950f830118af
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-11-30 12:21:00 +0000

    bump to v0.2.0

---
+++ release_version.sh
@@ -0,0 +1,28 @@
+VERSION=$1
+CRATES=(
+    "tandem"
+    "tandem_garble_interop"
+    "tandem_http_client"
+    "tandem_http_server"
+)
+for TOML in "${CRATES[@]}"; do
+    SED_COMMAND="3s/^version = "[^,]*"/version = \"$VERSION\"/"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "$SED_COMMAND" "${TOML}/Cargo.toml"
+    else
+        sed -i "$SED_COMMAND" "${TOML}/Cargo.toml"
+    fi
+    for CRATE in "${CRATES[@]}"; do
+        SED_COMMAND="s/$CRATE = { version = "[^,]*"/$CRATE = { version = \"$1\"/"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            sed -i '' -e "$SED_COMMAND" "$TOML/Cargo.toml"
+        else
+            sed -i "$SED_COMMAND" "$TOML/Cargo.toml"
+        fi
+    done
+done
+
+cargo build --all-features && \
+    git commit -am "bump to v$VERSION" && \
+    git tag -a "v$VERSION" -m "v$VERSION" && \
+    git push --tag
