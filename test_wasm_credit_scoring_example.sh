a95514a46e6d758bdbd77cf87fcdd205d429838b
Frederic Kettelhoit <kettelhoit@gmail.com>
2023-01-25 18:55:36 +0000

    test(http_client): run wasm tests using firefox geckodriver on macOS

--- test_wasm_credit_scoring_example.sh
+++ test_wasm_credit_scoring_example.sh
@@ -14,7 +14,7 @@ cp -r ./tandem_http_client/tests/credit_scoring_setup/* ./
 sleep 10
 if [[ "$OSTYPE" == "darwin"* ]]; then
     WASM_BINDGEN_TEST_TIMEOUT=300 wasm-pack test --release --headless \
-    --safari tandem_http_client --test credit_scoring
+    --firefox tandem_http_client --test credit_scoring
 else
     WASM_BINDGEN_TEST_TIMEOUT=300 wasm-pack test --release --headless \
     --chrome tandem_http_client --test credit_scoring
--- test_wasm_smart_cookie_example.sh
+++ test_wasm_smart_cookie_example.sh
@@ -14,7 +14,7 @@ cp -r ./tandem_http_client/tests/smart_cookie_setup/* ./
 sleep 10
 if [[ "$OSTYPE" == "darwin"* ]]; then
     WASM_BINDGEN_TEST_TIMEOUT=300 wasm-pack test --release --headless \
-    --safari tandem_http_client --test smart_cookie
+    --firefox tandem_http_client --test smart_cookie
 else
     WASM_BINDGEN_TEST_TIMEOUT=300 wasm-pack test --release --headless \
     --chrome tandem_http_client --test smart_cookie
